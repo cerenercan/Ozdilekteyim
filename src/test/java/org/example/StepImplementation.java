09fd7280e84e35f19aebe1e2d9dc3db5e076ed63
cerenercan <ceren.ercan@testinium.com>
2021-10-30 17:25:15 +0300

    changes

--- src/test/java/org/example/StepImplementation.java
+++ src/test/java/org/example/StepImplementation.java
@@ -18,31 +18,21 @@ import java.util.concurrent.TimeUnit;
 
 public class StepImplementation extends BaseTest{
 
-    @Step("<time> saniye bekle")
-    public void waitSeconds(int seconds) {
-        try {
-            TimeUnit.SECONDS.sleep(seconds);
-        } catch (InterruptedException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
     @Step("<Key> İd'li elemente tıkla")
     public void clickElementByid(String Key) {
         clickElement(By.id(Key));
         System.out.println(Key + "Elementine tiklandi");
     }
 
-    @Step("<Key> İd'li elemente <keywordc> değerini yaz")
-    public void SendkeyElementByid(String Key, String keyword) {
-        appiumDriver.findElement(By.id(Key)).sendKeys(keyword);
-        System.out.println(Key + "Elementine tiklandi");
-    }
-
     @Step("<Key> xpath'li elemente tıkla")
     public void clickElementByXpath(String Key) {
         clickElement(By.xpath(Key));
-        //appiumDriver.findElement(By.xpath(Key)).click();
+        System.out.println(Key + "Elementine tiklandi");
+    }
+
+    @Step("<Key> İd'li elemente <keywordc> değerini yaz")
+    public void SendkeyElementByid(String Key, String keyword) {
+        appiumDriver.findElement(By.id(Key)).sendKeys(keyword);
         System.out.println(Key + "Elementine tiklandi");
     }
 
@@ -70,6 +60,16 @@ public class StepImplementation extends BaseTest{
         productList.get(randomInt).click();
 
     }
+
+    @Step("<time> saniye bekle")
+    public void waitSeconds(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     @Step("Sepete ekle")
     public void addToBasket(){
 
@@ -88,16 +88,18 @@ public class StepImplementation extends BaseTest{
         String jsStmt = String.format("window.scrollTo(%d, %d);", x, y);
         getJSExecutor().executeScript(jsStmt, true);
     }
-    public void waitUntilPresence(By by) {
-        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(by));
-    }
-    public void waitUntilElementClickable(By by) {
-        System.out.println(by.toString()+" -> elementinin tiklanabilir olmasi bekleniyor");
-        wait.until(ExpectedConditions.elementToBeClickable(by));
-    }
 
     public WebElement findElement(By locator) {
         return appiumDriver.findElement(locator);
+    }
+
+    public void waitUntilPresence(By by) {
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(by));
+    }
+
+    public void waitUntilElementClickable(By by) {
+        System.out.println(by.toString()+" -> elementinin tiklanabilir olmasi bekleniyor");
+        wait.until(ExpectedConditions.elementToBeClickable(by));
     }
 
     public void clickElement(By by) {
